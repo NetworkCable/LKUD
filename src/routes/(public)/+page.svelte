cf057afba2c9d9de5450de5deb3559af8e726f4c
unknown <shapu953@gmail.com>
2022-10-21 19:12:49 +0530

    add: Container

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,15 +1,18 @@
 <script>
 	import Button from '$lib/components/common/Button.svelte';
+	import Container from '$lib/components/common/Container.svelte';
 </script>
 
-<div class="text-2xl font-bold font-monts">Lankan urban dict</div>
+<Container>
+	<div class="text-2xl font-bold font-monts">Lankan urban dict</div>
 
-<div class="w-full max-w-md flex flex-col gap-3">
-	<a href="/new">
-		<Button>Define a word</Button>
-	</a>
+	<div class="w-full max-w-md flex flex-col gap-3">
+		<a href="/new">
+			<Button>Define a word</Button>
+		</a>
 
-	<a href="/profile">
-		<Button>Profile</Button>
-	</a>
-</div>
+		<a href="/profile">
+			<Button>Profile</Button>
+		</a>
+	</div>
+</Container>
