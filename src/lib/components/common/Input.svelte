d7396bb3b81f49ba9558473ae1f60dda60f39c68
unknown <shapu953@gmail.com>
2022-10-24 22:59:31 +0530

    modify: input component

--- src/lib/components/common/Input.svelte
+++ src/lib/components/common/Input.svelte
@@ -1,7 +1,13 @@
 <script>
+	// @ts-nocheck
+
+	export let data = '';
 </script>
 
 <input
 	type="text"
 	class="h-12 w-full rounded-md border-solid border-lkud-red-primary  bg-lkud-yellow-primary"
+	bind:value={data}
 />
+
+<p>{data}</p>
