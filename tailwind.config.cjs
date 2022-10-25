c971925465032ea32b4d9f6bdeb398cc1ed38040
Menake <menake.j@jgsw.global>
2022-10-25 12:48:31 +0530

    add: tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        'monts': "Montserrat"
-      },
-      colors: {
-        "lkud-red": {
-          primary: "#8D153A"
-        },
-        "lkud-yellow": {
-          primary: "#FFBE29"
-        }
-      }
-    },
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				monts: 'Montserrat'
+			},
+			colors: {
+				'lkud-red': {
+					primary: '#8D153A',
+					secondary: '#D98484'
+				},
+				'lkud-yellow': {
+					primary: '#FFBE29'
+				},
+				'lkud-green': {
+					primary: '#00722A'
+				},
+				'lkud-gray': {
+					primary: '#C4C4C4'
+				}
+			}
+		}
+	},
+	plugins: []
+};
